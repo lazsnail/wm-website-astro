e0bc157d32d0c14f8c88e6832fe80f0816583a2e
Larry <larrybain01@gmail.com>
2024-03-27 20:17:18 +1000

    Removed SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,5 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
   integrations: [tailwind(), react()],
-  adapter: vercel()
 });
